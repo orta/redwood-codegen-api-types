e3937774685712167064b74e07755ecb0a914877
Orta Therox <git@orta.io>
2022-11-29 17:57:29 +1100

    Start of shared

---
+++ sharedSchema.ts
@@ -0,0 +1,7 @@
+/// The main schema for objects and inputs
+
+import { graphql } from "./deps.ts";
+
+const createSharedSchemaFiles = (schema: graphql.GraphQLSchema) => {
+  // We want to make a file with everything except Query and Mutation in it.
+};
